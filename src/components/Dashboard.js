4b8db472d313138e8e26dd2214cd39ab93322c1e
Daniel López <danilopez.pc@gmail.com>
2023-01-12 19:28:26 +0100

    Show polls by user logged

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { receiveQuestions } from "../actions/questionAction";
 import { _getQuestions } from "../utils/_DATA";
 import Login from "./Login";
@@ -9,6 +10,17 @@ const Dashboard = () => {
 	const dispatch = useDispatch();
 	const authedUser = useSelector(state => state.authedUser);
 	const questionsState = useSelector(state => state.questions);
+	const usersState = useSelector(state => state.users);
+
+	const questionsAnswered = Object.values(questionsState).filter((item)=>{
+		if(item.optionOne.votes.includes(authedUser) || item.optionTwo.votes.includes(authedUser))
+			return item;
+	})
+
+	const questionsNotAnswered = Object.values(questionsState).filter((item)=>{
+		if(!item.optionOne.votes.includes(authedUser) && !item.optionTwo.votes.includes(authedUser))
+			return item;
+	})
 	
 	useEffect(() => {
 		_getQuestions().then((questions)=>{
@@ -16,13 +28,13 @@ const Dashboard = () => {
 		})
 	},[dispatch]);
 
-	function formatDate(time){
+	function formatDate(time) {
 		const dateFormat= new Date(time);
 		const dateFormated = dateFormat.getDate()+
 		"/"+(dateFormat.getMonth()+1)+
 		"/"+dateFormat.getFullYear()+
 		" "+dateFormat.getHours()+
-		":"+dateFormat.getMinutes()
+		":"+dateFormat.getMinutes();
 		return dateFormated;
 	}
 
@@ -33,12 +45,12 @@ const Dashboard = () => {
 				<h1>New questions</h1>
 				<div className="pollListContainer">
 				{
-					questionsState && Object.values(questionsState).map((question) => {
+					questionsNotAnswered && questionsNotAnswered.map((question) => {
 						return (
 							<div className="pollContainerDetail">
 							<div>{question.author}</div>
 							<div>{formatDate(question.timestamp)}</div>
-							<button>View</button>
+							<Link to={`/questions/${question.id}`}><button>View</button></Link>
 					</div>
 						)
 					})
@@ -48,21 +60,17 @@ const Dashboard = () => {
 			<section className="pollSectionContainer">
 				<h1>Done</h1>
 				<div className="pollListContainer">
-					<div className="pollContainerDetail">
-						<div>Person</div>
-						<div>Date</div>
-						<button>View</button>
+				{
+					questionsAnswered && questionsAnswered.map((question) => {
+						return (
+							<div className="pollContainerDetail">
+							<div>{question.author}</div>
+							<div>{formatDate(question.timestamp)}</div>
+							<Link to={`/questions/${question.id}`}><button>View</button></Link>
 					</div>
-					<div className="pollContainerDetail">
-						<div>Person</div>
-						<div>Date</div>
-						<button>View</button>
-					</div>
-					<div className="pollContainerDetail">
-						<div>Person</div>
-						<div>Date</div>
-						<button>View</button>
-					</div>
+						)
+					})
+				}
 				</div>
 			</section>
 		</div>) : <Login />
